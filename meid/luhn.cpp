39e466f27d738d894a03f3cfeaa842d8be14a902
Butiaev. Aleksei <aleksei@ceranext.com>
2018-01-08 14:25:13 +0200

    non functional changes

--- meid/luhn.cpp
+++ meid/luhn.cpp
@@ -4,30 +4,30 @@
 template<unsigned int... S>
 struct luhn_data
 {
-  static const unsigned int data[2][sizeof...(S)];
-  static const unsigned int size = sizeof...(S);
+  static const unsigned int size = sizeof ...(S);
+  static const unsigned int data[2][size];
 };
 
 template<unsigned int... S>
-const unsigned int luhn_data<S...>::data[2][sizeof...(S)] = {
+const unsigned int luhn_data<S ...>::data[2][luhn_data<S ...>::size] = {
   {S...},
-  {S * 2 % luhn_data<S...>::size + S * 2 / luhn_data<S...>::size ...}
+  {S * 2 % luhn_data<S ...>::size + S * 2 / luhn_data<S ...>::size ...}
 };
 
 template<unsigned int I, unsigned int... S>
-struct luhn : luhn<I - 1, I - 1, S...>
+struct luhn : luhn<I - 1, I - 1, S ...>
 {};
 
 template<unsigned int... S>
-struct luhn<0, S...> : luhn_data<S...>
+struct luhn<0, S ...> : luhn_data<S...>
 {
 
-  using luhn_data<S...>::data;
-  using luhn_data<S...>::size;
+  using luhn_data<S ...>::data;
+  using luhn_data<S ...>::size;
 
-  static int get_cb(unsigned long long value)
+  static unsigned int get_cb(unsigned long long value)
   {
-    int sum = 0;
+    unsigned int sum = 0;
     int index = 0;
     while(value){
       sum += data[++index % 2][value % size];
@@ -41,7 +41,6 @@ struct luhn<0, S...> : luhn_data<S...>
 using luhn10 = luhn<10>;
 using luhn16 = luhn<16>;
 
-
 int main(int, char**)
 {
   std::cout << "dec table" << std::endl;
@@ -51,16 +50,14 @@ int main(int, char**)
   std::copy(std::begin(luhn10::data[1]), std::end(luhn10::data[1]), std::ostream_iterator<int>(std::cout, " "));
 
   std::cout << std::endl;
-    std::cout << "hex table" << std::endl << "0x" << std::hex;
+  std::cout << "IMEI 35948605448562/" << luhn10::get_cb(35948605448562) << std::endl;
+
+  std::cout << std::endl;
+  std::cout << "hex table" << std::endl << "0x" << std::hex;
 
   std::copy(std::begin(luhn16::data[0]), std::end(luhn16::data[0]), std::ostream_iterator<int>(std::cout, " 0x"));
   std::cout << std::endl << "0x";
   std::copy(std::begin(luhn16::data[1]), std::end(luhn16::data[1]), std::ostream_iterator<int>(std::cout, " 0x"));
-
-  std::cout << std::dec << std::endl;
-
-  std::cout << std::endl;
-  std::cout << "IMEI 35948605448562/" << luhn10::get_cb(35948605448562) << std::endl;
 
   std::cout << std::endl;
   std::cout << "MEID A000001D4FEC64/" << luhn16::get_cb(0xA000001D4FEC64) << std::endl;
