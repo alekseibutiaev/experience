120a6698eb9b9a0d29d97a0b75876454dcb33e8c
Butiaev. Aleksei <aleksei@ceranext.com>
2018-01-11 17:01:23 +0200

    update library, implementation filehash

---
+++ filehash/filehash.cpp
@@ -0,0 +1,233 @@
+#include <map>
+#include <mutex>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <stdexcept>
+#include <functional>
+#include <condition_variable>
+
+#include "logger.hpp"
+#include "thread_pool.hpp"
+
+namespace {
+
+/* b363e60ba48a594aa46a2081b81ea91f8d062656  ./kpx979.pdf */
+
+  const unsigned int default_block_size = 1024 * 1024 * 1;
+
+  using sha1buff_t = unsigned char[20];
+
+  class thread_pool : tools::thread_pool<16> {
+
+  public:
+
+    using parent = tools::thread_pool<16>;
+    using parent::start;
+	  using parent::stop;
+    using parent::execute;
+
+  private:
+
+    virtual void prepare_exception(const std::string& value) override {
+      logout(value);
+    }
+  };
+
+  class part_hasher {
+
+  public:
+
+    using notifier_t = std::function<void(unsigned int, const sha1buff_t&)>;
+    using buffer_t = std::vector<char>;
+
+  public:
+
+    part_hasher(const std::string& filename, unsigned int block_size,
+          unsigned int index, notifier_t notifier)
+        : m_index(index)
+        , m_buff(block_size, 0)
+        , m_notifier(notifier) {
+      std::ifstream stream(filename, std::ifstream::binary);
+      if(!stream)
+        throw std::runtime_error(std::to_string(__LINE__) + " can`t open file: " + filename);
+      unsigned int readfrom = block_size * index;
+      stream.seekg(readfrom, std::ifstream::beg);
+      m_data_size = stream.readsome(m_buff.data(), block_size);
+      logout("read: ", m_data_size, " from file: ", filename, " from position: ", readfrom, tools::logger::endl);
+    }
+
+    void operator()() const {
+      sha1buff_t result;
+      get_hash(m_buff, m_data_size, result);
+      m_notifier(m_index, result);
+    }
+
+  private:
+
+    static void get_hash(const buffer_t& data, const unsigned int size,
+      sha1buff_t& result) {
+    }
+
+  private:
+
+    unsigned int m_index;
+    notifier_t m_notifier;
+    buffer_t m_buff;
+    unsigned int m_data_size;
+
+  };
+
+  class hash_store {
+
+  public:
+
+    using notifier_t = std::function<void()>;
+
+  public:
+
+    hash_store(const std::string& file_name, unsigned int parts, notifier_t done)
+        : m_parts(parts)
+        , m_done(done)
+        , m_out(file_name) {
+      if(!m_out)
+        throw std::runtime_error(std::to_string(__LINE__) + " can`t open file: " + file_name);
+      m_out << std::hex;
+    }
+
+    void store(unsigned int index, const sha1buff_t& value) {
+      logout("Store: ", index, tools::logger::endl);
+      std::lock_guard<std::mutex> _(m_mtx);
+      std::copy(std::begin(value), std::end(value), std::begin(m_order[index]));
+      while(m_order.end() != m_order.find(m_last_store)) {
+        logout("write pos: ", m_last_store, tools::logger::endl);
+        store_to_file(m_order[m_last_store]);
+        m_order.erase(m_last_store);
+        ++m_last_store;
+        if(m_parts <= m_last_store)
+          m_done();
+      }
+    }
+
+  private:
+
+    using hash_order_t = std::map<unsigned int, sha1buff_t>;
+
+  private:
+
+    void store_to_file(const sha1buff_t& value) {
+      std::copy(std::begin(value), std::end(value), std::ostream_iterator<unsigned int>(m_out,""));
+      m_out << std::endl;
+    }
+
+  private:
+
+    const unsigned int m_parts;
+    notifier_t m_done;
+    std::ofstream m_out;
+    unsigned int m_last_store = 0;
+    hash_order_t m_order;
+    std::mutex m_mtx;
+
+  };
+
+  void get_param(int ac, char* av[], std::string& input, std::string& output,
+      unsigned int& block_size) {
+
+    static const std::runtime_error invalid_param(
+      std::string("ussage: ") + av[0] + " <input file> <output file> <block size>"
+        "\n  block size by default = " + std::to_string(default_block_size)
+    );
+
+    if(ac < 3)
+      throw invalid_param;
+    input = av[1];
+    output = av[2];
+    block_size = default_block_size;
+    if(ac >= 4) {
+      try {
+        block_size = std::stol(av[3]);
+      }
+      catch(const std::exception&){
+        throw invalid_param;
+      }
+    }
+  }
+
+  unsigned int get_block_count(const std::string& filename,
+      const unsigned int block_size) {
+    logout(filename, tools::logger::endl);
+    std::ifstream file(filename, std::ifstream::binary);
+    if(!file)
+       throw std::runtime_error(std::to_string(__LINE__) + " can`t open file: " + filename);
+    file.seekg(0, std::ifstream::end);
+    const std::ifstream::streampos size = file.tellg();
+    file.seekg(0, std::ifstream::beg);
+    return size / block_size + (0 != size % block_size);
+  }
+
+  struct waiter {
+    bool m_flag = false;
+    std::mutex m_mtx;
+    std::condition_variable m_cv;
+  };
+
+} /* namespace */
+
+
+int main(int ac, char* av[]) {
+
+  try {
+
+    std::string input;
+    std::string output;
+    unsigned int block_size;
+    get_param(ac, av, input, output, block_size);
+
+    logout("input file: ", input, tools::logger::endl);
+    logout("output file: ", output, tools::logger::endl);
+    logout("block size: ", block_size, tools::logger::endl);
+
+    unsigned int blocks = get_block_count(input, block_size);
+    if(!blocks)
+      throw std::runtime_error("file: " + input + " is empty.");
+
+
+    logout("blocks count: ", blocks, tools::logger::endl);
+
+    waiter w;
+    hash_store hs(output, blocks, [&w](){
+      std::lock_guard<std::mutex> _(w.m_mtx);
+      w.m_flag = true;
+      w.m_cv.notify_one();
+    });
+    thread_pool tp;
+    tp.start();
+    for(unsigned int index = 0; index < blocks; ++index)
+      tp.execute([&input, block_size, index, &hs](){
+        logout("prepare block: ", index, tools::logger::endl);
+        part_hasher ph(input, block_size, index,
+          std::bind(&hash_store::store, std::ref(hs), std::placeholders::_1,
+            std::placeholders::_2));
+        ph();
+      });
+    std::unique_lock<std::mutex> _(w.m_mtx);
+    while(!w.m_flag)
+      w.m_cv.wait(_);
+    tp.stop();
+
+    logout("finished", tools::logger::endl);
+    return -1;
+
+  }
+  catch(const std::exception& e) {
+    logout( e.what(), tools::logger::endl);
+  }
+  catch(...) {
+    logout( "unknown error.", tools::logger::endl);
+  }
+  return -1;
+}
+
--- meid/luhn.cpp
+++ meid/luhn.cpp
@@ -33,7 +33,7 @@ struct luhn<0, S ...> : luhn_data<S...>
       sum += data[++index % 2][value % size];
       value /= size;
     }
-    return size - sum % size;
+    return (size - sum % size) % size;
   }
 
 };
@@ -51,6 +51,10 @@ int main(int, char**)
 
   std::cout << std::endl;
   std::cout << "IMEI 35948605448562/" << luhn10::get_cb(35948605448562) << std::endl;
+  std::cout << "IMEI 33075934001217/" << luhn10::get_cb(33075934001217) << std::endl;
+  std::cout << "MEID 268435458312832709/" << luhn10::get_cb(268435458312832709) << std::endl;
+  std::cout << "MEID 268435458905237860/" << luhn10::get_cb(268435458905237860) << std::endl;
+  std::cout << "MEID 293608736500703710/" << luhn10::get_cb(293608736500703710) << std::endl;
 
   std::cout << std::endl;
   std::cout << "hex table" << std::endl << "0x" << std::hex;
@@ -61,6 +65,8 @@ int main(int, char**)
 
   std::cout << std::endl;
   std::cout << "MEID A000001D4FEC64/" << luhn16::get_cb(0xA000001D4FEC64) << std::endl;
+  std::cout << "MEID A0000017C3CFC5/" << luhn16::get_cb(0xA0000017C3CFC5) << std::endl;
+  std::cout << "MEID AF0123450ABCDE/" << luhn16::get_cb(0xAF0123450ABCDE) << std::endl;
 
   return 0;
 }
