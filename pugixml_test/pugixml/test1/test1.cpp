be7be640cd4e35407f7c6733cf4953a9fe6e933d
butiaev.aleksei <a.butyaev@tradernet.com>
2019-10-21 10:43:50 +0300

    implementation

--- pugixml_test/pugixml/test1/test1.cpp
+++ pugixml_test/pugixml/test1/test1.cpp
@@ -1,6 +1,27 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+
+#include <pugixml.hpp>
 
 int main(int ac, char* av[]) {
-  std::cout << "project" << std::endl;
-  return 0;
+  std::cout << av[1] << std::endl;
+  std::ifstream ifs(av[1]);
+  if(!ifs)
+    return EXIT_FAILURE;
+  //"module[@type=\"QSRV\"]"
+  try {
+    pugi::xml_document doc;
+    if(const auto& res = doc.load(ifs)) {
+
+    }
+    else
+      throw(std::runtime_error(res.description()));
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return EXIT_SUCCESS;
 }
