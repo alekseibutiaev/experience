1bc91e163866aeeb5a5a2c2f3d0d5c06b1ce2e3b
butiaev.aleksei <a.butyaev@tradernet.com>
2019-10-22 12:33:04 +0300

    improve more

--- pugixml_test/pugixml/test1/test1.cpp
+++ pugixml_test/pugixml/test1/test1.cpp
@@ -5,9 +5,11 @@
 
 #include <pugixml.hpp>
 
-template<typename type_t>
-void foo(const type_t& f) {
-
+void foo(const pugi::xml_node& f) {
+  if(f)
+    f.print(std::cout);
+  else
+    std::cout << "empty" << std::endl;
 }
 
 int main(int ac, char* av[]) {
@@ -19,9 +21,12 @@ int main(int ac, char* av[]) {
   try {
     pugi::xml_document doc;
     if(const auto& res = doc.load(ifs)) {
-      const pugi::xml_node& node = doc.first_child();
-      const auto& ppp = node.select_node("module[@type=\"QSRV\"]");
-      foo(ppp);
+      const pugi::xml_node node = doc.first_child();
+      foo(node);
+      const pugi::xml_node node1 = node.child("cfg1");
+      foo(node1);
+      const pugi::xml_node node2 = node1.select_node("module[@type=\"QSRV\"]").node();
+      foo(node2);
     }
     else
       throw(std::runtime_error(res.description()));
