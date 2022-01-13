d0c49d78532819f0f7a16ab2a39e6cc04340e4d3
Butiaev Aleksei <a.butyaev@tradernet.com>
2022-01-13 12:56:11 +0200

    add vecvsdec

--- boost-python-examples-master/15-CallPython/callpython.cpp
+++ boost-python-examples-master/15-CallPython/callpython.cpp
@@ -24,15 +24,16 @@ int main(int ac, char* av[]) {
     py_initialize_t ini;
     // Add the current directory to the path.
     boost::python::import("sys").attr("path").attr("append")(".");
-    // Get the "hello_from_python" function from "hello.py".
-    boost::python::object func = boost::python::import("pyprog").attr("get_message");
+    // Get the "get_message" and "get_message_param" function from "hello.py".
+    boost::python::object func0 = boost::python::import("pyprog").attr("get_message");
     boost::python::object func1 = boost::python::import("pyprog").attr("get_message_param");
-    // Call the "hello_from_python" and extract the return value as a string.
-    std::string return_value = boost::python::extract<std::string>(func());
-    std::string param = boost::python::extract<std::string>(func1("parametr"));
+    // Call the "get_message" and extract the return value as a string.
+    std::string res0 = boost::python::extract<std::string>(func0());
+    // Call the "get_message_param" and extract the return value as a string.
+    std::string res1 = boost::python::extract<std::string>(func1("parametr"));
     // Print out the return value.
-    std::cout << "Return value: '" << return_value << "'" << std::endl;
-    std::cout << "Return value: '" << param << "'" << std::endl;
+    std::cout << "Return value: '" << res0 << "'" << std::endl;
+    std::cout << "Return value: '" << res1 << "'" << std::endl;
   }
   catch (boost::python::error_already_set& e) {
     PyErr_PrintEx(0);
