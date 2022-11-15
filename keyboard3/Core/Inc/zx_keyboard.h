37ba5eb1d14e377528599d8aaf2de46c20321951
Butiaev Aleksei <alexsyreserv@mail.ru>
2022-11-15 17:55:11 +0200

    add symbol shift

--- keyboard3/Core/Inc/zx_keyboard.h
+++ keyboard3/Core/Inc/zx_keyboard.h
@@ -8,6 +8,8 @@
 #ifndef INC_ZX_KEYBOARD_H_
 #define INC_ZX_KEYBOARD_H_
 
+#define SYMBOL_SHIFT 232
+
 #include "usbh_hid_keyboard.h"
 
 void prepare_keys(const key_receive_t* keys, const key_leds_t* leds);
