6d0da57d5a851a7c00e3556dbb4bbf91ba441faf
Butiaev Aleksei <a.butyaev@tradernet.com>
2023-12-11 11:21:26 +0200

    some update

--- ncds/avrotest/avrotest.cpp
+++ ncds/avrotest/avrotest.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include <fstream>
 #include <sstream>
 #include <iomanip>
@@ -13,29 +14,29 @@
 #include <avro/Schema.hh>
 #include <avro/ValidSchema.hh>
 #include <avro/Compiler.hh>
-#include "avro/Compiler.hh"
-#include "avro/Encoder.hh"
-#include "avro/Decoder.hh"
-#include "avro/Specific.hh"
+#include <avro/Encoder.hh>
+#include <avro/Decoder.hh>
+#include <avro/Specific.hh>
 
 
 #include <print_records.h>
 
 namespace {
 
-  using pair_t = std::pair<avro::ValidSchema, avro::GenericDatum>;
   using buffer_t = std::vector<unsigned char>;
   using buffer_try_t = std::optional<buffer_t>;
   using valid_schema_ptr = std::shared_ptr<avro::ValidSchema>;
 
+  valid_schema_ptr load(std::istream& is) {
+    std::string error;
+    valid_schema_ptr schema = std::make_shared<valid_schema_ptr::element_type>();
+    avro::compileJsonSchema(is, *schema, error);
+    return schema;
+  }
 
   valid_schema_ptr load(const std::string& file) {
-    if(auto ifs = std::ifstream(file.c_str())) {
-      std::string error;
-      valid_schema_ptr schema = std::make_shared<valid_schema_ptr::element_type>();
-      avro::compileJsonSchema(ifs, *schema, error);
-      return schema;
-    }
+    if(auto ifs = std::ifstream(file.c_str()))
+      return load(ifs);
     throw(std::runtime_error("can`t open file: " + file));
   }
 
@@ -54,6 +55,7 @@ namespace {
 int main(int ac, char* av[]) {
   try {
     std::cout << "test " << __cplusplus << std::endl;
+    valid_schema_ptr totalview;
 
     auto schema = load("./ncdsresources/ControlMessageSchema.avsc");
 //    auto schema = load("./schema/QBBO-A-BSX.json");
@@ -62,65 +64,79 @@ int main(int ac, char* av[]) {
 
     std::cout << schema->toJson() << std::endl;
 
-    std::size_t idx = 0;
+    std::size_t idx = 540;
     buffer_t::value_type max = 0;
-    if(auto buf = load("./schema/", idx++, ".sch")) {
-      pbuffer(buf->data(), buf->size());
-      auto data = std::make_shared<avro::GenericDatum>(*schema);
-      auto in = avro::memoryInputStream(buf->data(), buf->size());
-      auto decoder = avro::binaryDecoder();
-      decoder->init(*in);
-      avro::decode(*decoder, *data);
-      const avro::GenericRecord& r = data->value<avro::GenericRecord>();
-      std::map<std::string, bool>
-      for (size_t i = 0; i < r.fieldCount(); i++) {
-        avro::GenericDatum datum = r.fieldAt(i);
-        
-        std::cout << r.schema()->nameAt(i) << ": ";
-        if (datum.type() == avro::AVRO_DOUBLE)
-          std::cout << datum.value<double>() << std::endl;
-        else if (datum.type() == avro::AVRO_LONG)
-          std::cout << datum.value<int64_t>() << std::endl;
-        else if (datum.type() == avro::AVRO_INT)
-          std::cout << datum.value<int>() << std::endl;
-        else if (datum.type() == avro::AVRO_STRING)
-          std::cout << datum.value<std::string>() << std::endl;
-      }
-
-
-    }
-
-#if 0
-
-//    while(const auto buf = load("/mnt/data1/butiaev/QBBO-A-BSX/QBBO-A-BSX.stream_", idx++, ".msg")) {
+    std::istringstream iss;
     while(auto buf = load("./schema/", idx++, ".sch")) {
-      pbuffer(buf->data(), buf->size());
-      avro::GenericDatum data(schema);
-      auto in = avro::memoryInputStream(buf->data(), buf->size());
-      auto decoder = avro::binaryDecoder();
-      decoder->init(*in);
-      avro::decode(*decoder, data);
-/*
-      const avro::GenericRecord& r = data.value<avro::GenericRecord>();
-      for (size_t i = 0; i < r.fieldCount(); i++) {
-        avro::GenericDatum datum = r.fieldAt(i);
-        std::cout << r.schema()->nameAt(i) << ": ";
-        if (datum.type() == avro::AVRO_DOUBLE)
-          std::cout << datum.value<double>() << std::endl;
-        else if (datum.type() == avro::AVRO_LONG)
-          std::cout << datum.value<int64_t>() << std::endl;
-        else if (datum.type() == avro::AVRO_INT)
-          std::cout << datum.value<int>() << std::endl;
-        else if (datum.type() == avro::AVRO_STRING)
-          std::cout << datum.value<std::string>() << std::endl;
+      try {
+        pbuffer(buf->data(), buf->size());
+        auto data = std::make_shared<avro::GenericDatum>(*schema);
+        auto in = avro::memoryInputStream(buf->data(), buf->size());
+        auto decoder = avro::binaryDecoder();
+        decoder->init(*in);
+        avro::decode(*decoder, *data);
+        const avro::GenericRecord& record = data->value<avro::GenericRecord>();
+        for (size_t i = 0; i < record.fieldCount(); i++) {
+          avro::GenericDatum datum = record.fieldAt(i);
+          std::cout << record.schema()->nameAt(i) << ": ";
+          if (datum.type() == avro::AVRO_DOUBLE)
+            std::cout << datum.value<double>() << std::endl;
+          else if (datum.type() == avro::AVRO_LONG)
+            std::cout << datum.value<int64_t>() << std::endl;
+          else if (datum.type() == avro::AVRO_INT)
+            std::cout << datum.value<int>() << std::endl;
+          else if (datum.type() == avro::AVRO_STRING)
+            std::cout << datum.value<std::string>() << std::endl;
+        }
+        if(record.field("name").value<std::string>() == "TOTALVIEW") {
+          iss = std::move(std::istringstream(record.field("schema").value<std::string>()));
+          break;
+        }
       }
-*/
+      catch(const std::exception& e) {
+        std::cout << e.what() << std::endl;
+      }
     }
-#endif
-      while(1)
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-      std::cout << idx << " " << std::hex << static_cast<int>(max) << std::endl;
-      return 0;
+    std::cout << iss.str() << std::endl;
+    if(auto twschema = load(iss)) {
+      std::size_t idx = 0;
+      std::cout << avro::toString(twschema->root()->type()) << " " << twschema->root()->leaves() << std::endl;
+      std::cout << twschema->toJson() << std::endl;
+      while(auto buf = load("/mnt/data1/butiaev/TOTALVIEW_1/TOTALVIEW.stream_", idx++, ".msg")) {
+        try {
+          pbuffer(buf->data(), buf->size());
+          auto data = std::make_shared<avro::GenericDatum>(*twschema);
+          auto in = avro::memoryInputStream(buf->data(), buf->size());
+          auto decoder = avro::binaryDecoder();
+          decoder->init(*in);
+          avro::decode(*decoder, *data);
+          const avro::GenericRecord& record = data->value<avro::GenericRecord>();
+          for (size_t i = 0; i < record.fieldCount(); i++) {
+            avro::GenericDatum datum = record.fieldAt(i);
+            std::cout << record.schema()->nameAt(i) << ": ";
+            if (datum.type() == avro::AVRO_DOUBLE)
+              std::cout << datum.value<double>() << std::endl;
+            else if (datum.type() == avro::AVRO_LONG)
+              std::cout << datum.value<int64_t>() << std::endl;
+            else if (datum.type() == avro::AVRO_INT)
+              std::cout << datum.value<int>() << std::endl;
+            else if (datum.type() == avro::AVRO_STRING)
+              std::cout << datum.value<std::string>() << std::endl;
+          }
+          std::cout << std::endl;
+        }
+        catch(const std::exception& e) {
+          std::cout << e.what() << std::endl;
+        }
+      }
+
+    }
+
+
+    while(1)
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << idx << " " << std::hex << static_cast<int>(max) << std::endl;
+    return 0;
   }
   catch(const std::exception& e) {
     std::cout << e.what() << std::endl;
