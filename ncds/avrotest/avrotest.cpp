30bd21934f4f0f587a149c6f7d9bea1ba2ae6bdf
Butiaev Aleksei <a.butyaev@tradernet.com>
2024-07-05 10:27:16 +0300

    add resource file

--- ncds/avrotest/avrotest.cpp
+++ ncds/avrotest/avrotest.cpp
@@ -21,7 +21,6 @@
 #include <avro/GenericDatum.hh>
 
 
-#include <print_records.h>
 
 namespace {
 
@@ -57,6 +56,38 @@ namespace {
       res = buf;
     }
     return res;
+  }
+
+  void pbuffer(const void* buf, const std::size_t& size) {
+    char hex[128] = {0};
+    char ch[17] = {0};
+    for(std::size_t i = 0; i < size; ++i) {
+      sprintf(&hex[(i % 16) * 5], "0x%02X ", reinterpret_cast<const unsigned char*>(buf)[i]);
+      sprintf(&ch[(i % 16)], "%c", std::isprint(reinterpret_cast<const unsigned char*>(buf)[i]) ?
+        reinterpret_cast<const unsigned char*>(buf)[i] : '.');
+      if(0 == (i + 1) % 16)
+        std::cout << hex << ch << std::endl;
+    }
+    if(size % 16)
+      std::cout << hex << ch << std::endl;
+  }
+
+  void print_records(const std::vector<avro::GenericRecord>& records, std::ostream& os) {
+    for (auto &record : records) {
+      os << "Message name: " << record.schema()->name().simpleName() << std::endl;
+      for (size_t i = 0; i < record.fieldCount(); i++) {
+        avro::GenericDatum datum = record.fieldAt(i);
+        os << record.schema()->nameAt(i) << ": ";
+        if (datum.type() == avro::AVRO_DOUBLE)
+          os << datum.value<double>() << std::endl;
+        else if (datum.type() == avro::AVRO_LONG)
+          os << datum.value<int64_t>() << std::endl;
+        else if (datum.type() == avro::AVRO_INT)
+          os << datum.value<int>() << std::endl;
+        else if (datum.type() == avro::AVRO_STRING)
+          os << datum.value<std::string>() << std::endl;
+      }
+    }
   }
 
   void evidence(const std::string& str, const buffer_try_t& buf) {
@@ -166,18 +197,24 @@ namespace {
 
 } /* namespace */
 
+#if 0
+const std::string sch = "./resources/ControlMessageSchema.avsc";
+const std::string msgb = "/home/butiaev/project/ncds/schema_out/schema_";
+const std::string msge = ".sch";
+#else
+const std::string sch = "./resources/TOTALVIEW.json";
+const std::string msgb = "/mnt/data/butiaev/TOTALVIEW/TOTALVIEW.stream_";
+const std::string msge = ".msg";
+#endif
+
 int main(int ac, char* av[]) {
   try {
-//    evidence("./schema/TOTALVIEW.json", load("/mnt/data/butiaev/TOTALVIEW/TOTALVIEW.stream_", 1, ".msg"));
     std::cout << "test " << __cplusplus << std::endl;
-    valid_schema_ptr schema = load_schema("./schema/TOTALVIEW.json");
-    const auto leaves = schema->root()->leaves();
-    const auto type = schema->root()->type();
-    std::cout << avro::toString(type) << " " << leaves << std::endl;
+    valid_schema_ptr schema = load_schema(sch);
     std::cout << avro::toString(schema->root()->type()) << " " << schema->root()->leaves() << std::endl;
     std::cout << schema->toJson() << std::endl;
-    std::size_t idx = 1;
-    while(buffer_try_t buf = load("/mnt/data/butiaev/TOTALVIEW/TOTALVIEW.stream_", idx++, ".msg")) {
+    std::size_t idx = 0;
+    while(buffer_try_t buf = load(msgb, idx++, msge)) {
       // show schema
       pbuffer(buf->data(), buf->size());
       auto data = std::make_shared<avro::GenericDatum>(*schema);
@@ -192,6 +229,7 @@ int main(int ac, char* av[]) {
   //    records.push_back(*data);
       print_records({r}, std::cout);
     }
+    std::cout << "tested: " << idx << " records" << std::endl;
   }
   catch(const std::exception& e) {
     std::cout << e.what() << std::endl;
